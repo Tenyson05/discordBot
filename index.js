a4f08a2dbdf413f0243c396630b3fc08bd71177d
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-07-06 10:23:31 -0700

    Added word filtering

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ bot.commands = new Enmap();
 
 const TOKEN = process.env.TOKEN;
 
-
-
-
 fs.readdir('./events/', (err, files) => {
 	if(err) return console.error;
 	files.forEach(file => {
@@ -42,51 +39,20 @@ fs.readdir("./commands/", (err, files) => {
 	})
 })
 
+var blocklist = ["badword", "badword1", "badword2"];
+
+bot.on("message", msg => {
+
+// check the msg for a blockList content
+		let foundWord = false;
+		blocklist.forEach((blocked, i) => {
+			if(msg.content.toLowerCase().includes(blocklist[i])) foundWord = true;
+		})
+
+		if(foundWord) {
+			msg.delete();
+			msg.channel.send("You cant say that mah dude");
+		}
+})
+
 bot.login(TOKEN);
-
-// require('dotenv').config();
-// const Discord = require('discord.js');
-// const bot = new Discord.Client();
-// bot.commands = new Discord.Collection();
-// const botCommands = require('./commands/exports');
-// const fs = require('fs');
-
-// Object.keys(botCommands).map(key => {
-// 	bot.commands.set(botCommands[key].name, botCommands[key]);
-// });
-
-// const TOKEN = process.env.TOKEN;
-// // const PREFIX = process.env.PREFIX;
-
-
-
-// fs.readdir('./events/', (err, files) => {
-// 	if(err) return console.error;
-// 	files.forEach(file => {
-// 		if(!file.endsWith('.js')) return;
-// 		const evt = require(`./events/${file}`);
-// 		let evtName = file.split('.')[0];
-// 		console.log(`Loaded '${evtName}'.`);
-// 		bot.on(evtName, evt.bind(null, bot));
-// 	});
-// });
-
-// bot.on('message', msg => {
-// 	const args = msg.content.split(/ +/);
-// 	const command = args.shift().toLowerCase();
-// 	console.info(`Called command: ${command}`);
-
-// 	if (!bot.commands.has(command)) return;
-// 	// if (msg.author.bot) return;
-// 	// if (msg.content.indexOf(PREFIX) !== 0) return "Missing prefix";
-	
-
-// 	try {
-// 		bot.commands.get(command).execute(msg, args);
-// 	} catch (error) {
-// 		console.error(error);
-// 		msg.reply('there was an error trying to execute that command!');
-// 	}
-// });
-
-// bot.login(TOKEN);
